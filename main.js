ef8f22ff4e85e0470597292ddc3fe919d0beaec3
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-05-27 13:33:44 -0600

    clase 27/05/24 introduccion a js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-//Programacion
+var edad=0
+
+function mostrar(){
+    if (edad>=18) {
+        alert("Biembenido a bodegas alianza...")        
+    }else{
+        alert("No tienes edad duficiente para comparar alcohol...")
+    }
+
+    array.forEach(Element=>{
+
+    })
+}
+
+function capturar(){
+    edad= prompt("Captura tu edad...")
+}
